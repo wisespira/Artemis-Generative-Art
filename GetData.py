50119246c6c70628c10d78ee503838d24b809369
Ira Watt <irawatt@gmail.com>
2021-01-03 21:30:29 +0000

    update

--- GetData.py
+++ GetData.py
@@ -1,11 +1,18 @@
 ## From Data
 import pandas as pd
 import numpy as np
+## ML
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import glob
 ## Web Scraping 
 import requests
 from bs4 import BeautifulSoup
 ## Image manipulation
 from PIL import Image
+from torchvision import transforms 
+import matplotlib.pyplot as plt
 ## Filing Functionality 
 import io
 import os
@@ -14,7 +21,60 @@ import hashlib
 
 
 im = Image.open("C:/Users/Admin/Desktop/ART/data/Abstract_gallery/Abstract_image_0.jpg")
-im.show()
+#im.show()
+width, height = im.size
+
+
+
+trans = transforms.ToPILImage()
+trans1 = transforms.ToTensor()
+print(trans1(im))
+trans(trans1(im)).show()
+#plt.imshow(trans(trans1(im)))
+
+
+#tf.reset_default_graph()
+
+#batch_size = 64
+
+#X_in = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='X')
+#Y    = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='Y')
+#Y_flat = tf.reshape(Y, shape=[-1, 28 * 28])
+#keep_prob = tf.placeholder(dtype=tf.float32, shape=(), name='keep_prob')
+
+#dec_in_channels = 1
+#n_latent = 8
+
+#reshaped_dim = [-1, 7, 7, dec_in_channels]
+#inputs_decoder = 49 * dec_in_channels / 2
+
+
+#def lrelu(x, alpha=0.3):
+#    return tf.maximum(x, tf.multiply(x, alpha))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
